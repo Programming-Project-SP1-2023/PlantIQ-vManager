875aec5bac6feac353570eda3c29fba7ae81e792
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-23 20:55:31 +1000

    - Created DashboardController.java & dashboard.fxml, the dashboard will simply contain empty anchor panes that scene components will be loaded into.

---
+++ src/main/java/com/plantiq/vsmarthomehub/controllers/DashboardController.java
@@ -0,0 +1,45 @@
+package com.plantiq.vsmarthomehub.controllers;
+
+import com.plantiq.vsmarthomehub.vManager;
+import com.plantiq.vsmarthomehub.services.AuthenticationService;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class DashboardController {
+
+
+    @FXML
+    AnchorPane navContainer;
+
+    @FXML
+    AnchorPane sideMenuContainer;
+
+    SideMenuController sideMenuController;
+
+
+    public void logoutButtonPress() throws IOException {
+
+        if(AuthenticationService.logout()){
+            vManager.getInstance().showLoginStage();
+            vManager.getStageById("dashboard").close();
+        }
+
+    }
+
+    @FXML
+    public void initialize() throws IOException {
+        FXMLLoader menuLoader = new FXMLLoader(vManager.class.getResource("navbar.fxml"));
+        AnchorPane menuNodes = menuLoader.load();
+        this.navContainer.getChildren().add(menuNodes.getChildren().get(0));
+
+        FXMLLoader sideMenuLoader = new FXMLLoader(vManager.class.getResource("sideMenu.fxml"));
+        AnchorPane sideMenuNodes = sideMenuLoader.load();
+        this.sideMenuContainer.getChildren().add(sideMenuNodes.getChildren().get(0));
+    }
+
+
+
+}
