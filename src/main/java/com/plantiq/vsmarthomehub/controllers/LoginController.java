86ebfe80ff64ae6e8627968447a5c27a3ca1a4b7
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-23 19:59:04 +1000

    - Created LoginController.java class, this is directly called by the login view and allows the user to login, if successful it will close the login stage and open the dashboard stage. - Added registrationButtonPress, this method will simply open the users browsers to the login page.

---
+++ src/main/java/com/plantiq/vsmarthomehub/controllers/LoginController.java
@@ -0,0 +1,49 @@
+package com.plantiq.vsmarthomehub.controllers;
+
+import com.plantiq.vsmarthomehub.HelloApplication;
+import com.plantiq.vsmarthomehub.services.AuthenticationService;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URI;
+
+
+public class LoginController {
+
+    @FXML
+    public TextField emailInputField;
+
+    @FXML
+    public PasswordField passwordInputField;
+
+    @FXML
+    public Label alert;
+
+    public void loginButtonPress() throws IOException {
+
+        if(!emailInputField.getText().isBlank() && !passwordInputField.getText().isBlank()){
+            if(AuthenticationService.login(emailInputField.getText(),passwordInputField.getText())){
+                HelloApplication.getInstance().showDashboardStage();
+                HelloApplication.getStageById("login").close();
+            }else{
+                alert.setText("Login failed, please check your email and password");
+                alert.setStyle("-fx-background-color:  #6b1a1a");
+                alert.setVisible(true);
+            }
+        }else{
+            alert.setText("Login failed, please your email & password are not blank");
+            alert.setStyle("-fx-background-color:  #a28b28");
+            alert.setVisible(true);
+        }
+
+    }
+
+    public void registrationButtonPress() throws IOException {
+        java.awt.Desktop.getDesktop().browse(URI.create("https://plantiq.azurewebsites.net/"));
+    }
+
+}
