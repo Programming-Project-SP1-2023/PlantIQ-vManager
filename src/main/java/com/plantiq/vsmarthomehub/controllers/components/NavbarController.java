4cf1d8980f6c7e63eb5fd13359b1b03f9b13d5d4
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-23 23:58:48 +1000

    - Updated NavbarController.java to now perform an initialize method and set the email text label to the value of the current user.

--- src/main/java/com/plantiq/vsmarthomehub/controllers/components/NavbarController.java
+++ src/main/java/com/plantiq/vsmarthomehub/controllers/components/NavbarController.java
@@ -2,16 +2,29 @@ package com.plantiq.vsmarthomehub.controllers.components;
 
 import com.plantiq.vsmarthomehub.vManager;
 import com.plantiq.vsmarthomehub.services.AuthenticationService;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class NavbarController {
+public class NavbarController implements Initializable {
 
+
+    @FXML
+    private Label currentUser;
 
     public void logoutButtonPress() throws IOException {
         if(AuthenticationService.logout()){
             vManager.getInstance().showLoginStage();
             vManager.getStageById("dashboard").close();
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.currentUser.setText(vManager.getInstance().getUser().getEmail());
     }
 }
