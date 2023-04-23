a70ef85bcbd6c2e42001107ce2bd8e9668d6e9db
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-23 20:05:46 +1000

    - Updated module-info to now require java.desktop, boostrapfx.core, java.net.http & org.json. - Updated module-info to now open the controllers package to JavaFX.fxml & export the controllers, model and services packages.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module com.plantiq.vsmarthomehub {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.net.http;
+    requires org.json;
 
     opens com.plantiq.vsmarthomehub to javafx.fxml;
+    opens com.plantiq.vsmarthomehub.controllers to javafx.fxml;
+
+    exports com.plantiq.vsmarthomehub.models;
     exports com.plantiq.vsmarthomehub;
+    exports com.plantiq.vsmarthomehub.controllers;
+    exports com.plantiq.vsmarthomehub.services;
 }
