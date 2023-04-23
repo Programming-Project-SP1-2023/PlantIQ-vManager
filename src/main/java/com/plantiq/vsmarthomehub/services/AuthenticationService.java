d3d9dfe394c58d2ccf3ab62a6babcedb1a8cc59d
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-23 23:57:25 +1000

    - Updated AuthenticationService to now load and set the user on login or valid token check!

--- src/main/java/com/plantiq/vsmarthomehub/services/AuthenticationService.java
+++ src/main/java/com/plantiq/vsmarthomehub/services/AuthenticationService.java
@@ -1,5 +1,7 @@
 package com.plantiq.vsmarthomehub.services;
 
+import com.plantiq.vsmarthomehub.models.User;
+import com.plantiq.vsmarthomehub.vManager;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -23,8 +25,10 @@ public class AuthenticationService {
 
         if(result.get("outcome").equals("true")){
             FileService.saveSession(result.getString("session"));
+            AuthenticationService.setUser(result.getString("session"));
             outcome = true;
         }
+
 
         return outcome;
     }
@@ -50,6 +54,7 @@ public class AuthenticationService {
 
         if((boolean) result.get("outcome")){
             outcome = true;
+            AuthenticationService.setUser(session);
         }
 
         return outcome;
@@ -69,6 +74,23 @@ public class AuthenticationService {
         System.out.println(response);
 
         return true;
+    }
+
+    private static void setUser(String token){
+        String response = null;
+        try {
+            response = HttpService.getRequest("https://api-plantiq.azurewebsites.net/user/session/"+token);
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if(response != null){
+            JSONObject result = new JSONObject(response);
+            result = result.getJSONObject("data");
+            vManager.getInstance().setUser(new User(result.getString("id"),result.getString("firstname"),result.getString("surname"),result.getString("email"), 0));
+        }else{
+            vManager.getInstance().setUser(new User("network_error","network_error","network_error","network@error.com", 0));
+        }
     }
 
 
