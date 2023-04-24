e590cca857b73549e49c280309f3f36a267e9196
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-24 12:06:22 +1000

    - Added new HBox called actionButtons, this will be returned to the table and contain all our action buttons. - Updated the start/stop button to now recolor its self to the correct style on state change. - Added call to refresh the table data on start/stop change. - Added viewButton & Delete button with their correct classes and size. - Added all buttons to the new actionButtons HBox.

--- src/main/java/com/plantiq/vsmarthomehub/models/SmartHomeHub.java
+++ src/main/java/com/plantiq/vsmarthomehub/models/SmartHomeHub.java
@@ -1,9 +1,13 @@
 package com.plantiq.vsmarthomehub.models;
 
 
+import com.plantiq.vsmarthomehub.controllers.components.VirtualSmartHomeHubController;
 import com.plantiq.vsmarthomehub.core.ModelCollection;
 import com.plantiq.vsmarthomehub.vManager;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 
 public class SmartHomeHub {
 
@@ -16,6 +20,12 @@ public class SmartHomeHub {
 
     private boolean running;
     private Button startButton;
+
+    private Button viewButton;
+
+    private Button deleteButton;
+
+    private HBox actionButtons;
 
     public static ModelCollection collection(){
         return new ModelCollection();
@@ -30,12 +40,18 @@ public class SmartHomeHub {
         this.virtual = virtual;
         this.running = vManager.getInstance().getRunningVirtualHubs().containsKey(id);
         String buttonValue;
+        String buttonClass;
         if(running){
             buttonValue = "Stop";
+            buttonClass = "btn-danger";
         }else{
             buttonValue = "Start";
+            buttonClass = "btn-success";
         }
         this.startButton = new Button(buttonValue);
+        this.startButton.getStyleClass().add(buttonClass);
+        this.startButton.getStyleClass().add("btn");
+        this.startButton.setPrefWidth(24);
         this.startButton.setOnAction(event -> {
 
             if(running){
@@ -45,16 +61,48 @@ public class SmartHomeHub {
                 }
                 this.running = false;
                 this.startButton.setText("Start");
+                this.startButton.getStyleClass().remove("btn-danger");
+                this.startButton.getStyleClass().add("btn-success");
 
             }else{
                 if(!vManager.getInstance().getRunningVirtualHubs().containsKey(id)){
                     vManager.getInstance().getRunningVirtualHubs().put(id,this);
                     this.running = true;
                     this.startButton.setText("Stop");
+                    this.startButton.getStyleClass().remove("btn-success");
+                    this.startButton.getStyleClass().add("btn-danger");
                 }
             }
 
+            //refresh this page if its open!
+            if(VirtualSmartHomeHubController.getInstance() != null){
+                VirtualSmartHomeHubController.getInstance().refreshTableData();
+            }
+
         });
+
+        this.viewButton = new Button("View");
+        this.viewButton.setPrefWidth(24);
+        this.viewButton.getStyleClass().add("btn");
+        this.viewButton.getStyleClass().add("btn-primary2");
+
+        this.deleteButton = new Button("Delete");
+        this.deleteButton.setPrefWidth(24);
+        this.deleteButton.getStyleClass().add("btn");
+        this.deleteButton.getStyleClass().add("btn-danger");
+
+        this.actionButtons = new HBox();
+        this.actionButtons.setSpacing(8);
+        HBox.setHgrow(this.actionButtons, Priority.ALWAYS);
+        Region region1 = new Region();
+        HBox.setHgrow(region1, Priority.ALWAYS);
+        Region region2 = new Region();
+        HBox.setHgrow(region2, Priority.ALWAYS);
+        this.actionButtons.getChildren().add(this.startButton);
+        this.actionButtons.getChildren().add(region1);
+        this.actionButtons.getChildren().add(this.viewButton);
+        this.actionButtons.getChildren().add(region2);
+        this.actionButtons.getChildren().add(this.deleteButton);
     }
 
     public String getId(){
@@ -77,11 +125,11 @@ public class SmartHomeHub {
         return virtual;
     }
 
-    public Button getStartButton(){
-        return this.startButton;
-    }
-
     public boolean getRunning(){
         return running;
+    }
+
+    public HBox getActionButtons(){
+        return this.actionButtons;
     }
 }
