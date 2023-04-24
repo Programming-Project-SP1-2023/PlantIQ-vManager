ecc2dd326ad476f4bcdaf071dee17677f0027c0c
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-24 12:04:40 +1000

    - Updated the ModelCollection class to now correctly append the query parameters to the url if present, currently this only supports where.

--- src/main/java/com/plantiq/vsmarthomehub/core/ModelCollection.java
+++ src/main/java/com/plantiq/vsmarthomehub/core/ModelCollection.java
@@ -8,21 +8,49 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ModelCollection {
 
     private HashMap<String,String> where;
 
+    public ModelCollection(){
+        this.where = new HashMap<>();
+    }
+
     public ModelCollection where(String id, String value){
-        //TODO add later
+        this.where.put(id,value);
         return this;
     }
 
     public ArrayList<SmartHomeHub> get(){
+
+        StringBuilder parameters = new StringBuilder();
+        AtomicInteger count = new AtomicInteger();
+
+        this.where.forEach((key,value)->{
+            if(count.getAndIncrement() == 0){
+               parameters.append("?");
+                parameters.append(key);
+                parameters.append("=");
+                parameters.append(value);
+            }else{
+                parameters.append("&");
+                parameters.append(key);
+                parameters.append("=");
+                parameters.append(value);
+            }
+        });
+
+        String URI = "https://api-plantiq.azurewebsites.net/smarthub/all";
+        if(!parameters.isEmpty()){
+            URI += parameters.toString();
+        }
+
         String response = null;
         ArrayList<SmartHomeHub> output = new ArrayList<>();
         try {
-            response = HttpService.getRequest("https://api-plantiq.azurewebsites.net/smarthub/all");
+            response = HttpService.getRequest(URI);
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
