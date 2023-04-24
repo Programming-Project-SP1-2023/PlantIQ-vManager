4ef7bc533c280dbdf8cd5a4da0d5ea352be264c4
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-24 12:45:00 +1000

    - Updated ActionColumn to now target the actionButtons HBox instead of a single button! - Added limit & offset Selector combo box with options! These will now call our new launch FetchSmartHubsTask method that allows us to update our data. - Added new launchFetchSmartHomeHubsTask method, this can be called by any caller in the controller and will update the values. - Added refreshTableData method, this will be called by a SmartHomeHub on a variable change to ensure it's correctly reflected in the table. - Added static getter for the instance, this is required to allow other objects without reference to call it.

--- src/main/java/com/plantiq/vsmarthomehub/controllers/components/VirtualSmartHomeHubController.java
+++ src/main/java/com/plantiq/vsmarthomehub/controllers/components/VirtualSmartHomeHubController.java
@@ -1,22 +1,18 @@
 package com.plantiq.vsmarthomehub.controllers.components;
 
 import com.plantiq.vsmarthomehub.models.SmartHomeHub;
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
+import com.plantiq.vsmarthomehub.tasks.FetchSmartHomeHubs;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class VirtualSmartHomeHubController {
 
+    private static VirtualSmartHomeHubController instance;
 
     @FXML
     private TableColumn<SmartHomeHub, Integer> lastPostedColumn;
@@ -30,40 +26,84 @@ public class VirtualSmartHomeHubController {
     @FXML
     private TableColumn<SmartHomeHub, Boolean> runningColumn;
 
+
     @FXML
-    private TableColumn<SmartHomeHub, Button> actionColumn;
+    private TableColumn<SmartHomeHub, HBox> actionColumn;
 
     @FXML
     private TableView<SmartHomeHub> table;
 
-    private ObservableList<SmartHomeHub> tableData;
+    @FXML
+    private ComboBox<Integer> limitSelector;
+
+    @FXML
+    private ComboBox<Integer> offsetSelector;
 
     @FXML
 
     public void initialize(){
+
+        VirtualSmartHomeHubController.instance = this;
+
+        this.table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
         this.nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         this.postFrequencyColumn.setCellValueFactory(new PropertyValueFactory<>("postFrequency"));
         this.lastPostedColumn.setCellValueFactory(new PropertyValueFactory<>("lastPosted"));
         this.runningColumn.setCellValueFactory(new PropertyValueFactory<>("running"));
-        this.actionColumn.setCellValueFactory(new PropertyValueFactory<>("startButton"));
+        this.actionColumn.setCellValueFactory(new PropertyValueFactory<>("actionButtons"));
+        this.actionColumn.setMinWidth(120);
 
-        this.tableData = FXCollections.observableArrayList();
-        this.updateTableData();
+        //Setup limit selector
+        this.limitSelector.getItems().add(5);
+        this.limitSelector.getItems().add(10);
+        this.limitSelector.getItems().add(25);
+        this.limitSelector.getItems().add(50);
+        this.limitSelector.getItems().add(75);
+        this.limitSelector.getItems().add(100);
+        this.limitSelector.setValue(25);
 
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                Platform.runLater(() -> {
-                    table.refresh();
-                });
-            }
-        }, new Date(), 1000);
+        this.limitSelector.valueProperty().addListener((observableValue, integer, t1) -> this.launchFetchSmartHomeHubsTask());
 
+
+        //Setup offset selector
+        this.offsetSelector.getItems().add(0);
+        this.offsetSelector.getItems().add(5);
+        this.offsetSelector.getItems().add(10);
+        this.offsetSelector.getItems().add(25);
+        this.offsetSelector.getItems().add(50);
+        this.offsetSelector.getItems().add(75);
+        this.offsetSelector.getItems().add(100);
+        this.offsetSelector.setValue(0);
+
+        this.offsetSelector.valueProperty().addListener((observableValue, integer, t1) -> this.launchFetchSmartHomeHubsTask());
+
+
+        this.launchFetchSmartHomeHubsTask();
     }
 
-    private void updateTableData(){
+    private void launchFetchSmartHomeHubsTask(){
+        FetchSmartHomeHubs task = new FetchSmartHomeHubs(this.limitSelector.getValue(),this.offsetSelector.getValue(),true);
 
-        this.table.setItems(FXCollections.observableList(SmartHomeHub.collection().get()));
+        task.valueProperty().addListener((observableValue, smartHomeHubs, t1) -> VirtualSmartHomeHubController.getInstance().setTableData(t1));
+        Thread thread = new Thread(task);
+
+        thread.setDaemon(true);
+        thread.start();
     }
+
+
+    public void setTableData(ObservableList<SmartHomeHub> data){
+        this.table.setItems(data);
+    }
+
+    public void refreshTableData(){
+        this.table.refresh();
+    }
+
+    public static VirtualSmartHomeHubController getInstance(){
+        return VirtualSmartHomeHubController.instance;
+    }
+
 
 }
