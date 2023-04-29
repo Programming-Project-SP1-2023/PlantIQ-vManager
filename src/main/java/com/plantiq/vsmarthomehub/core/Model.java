c6ac2d7377039004b2199a227887027fb6a256c7
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-29 20:00:52 +1000

    - Updated Model class to now correctly accept and store the JSON data. - Added getData method that will return the JSON object.

--- src/main/java/com/plantiq/vsmarthomehub/core/Model.java
+++ src/main/java/com/plantiq/vsmarthomehub/core/Model.java
@@ -1,4 +1,16 @@
 package com.plantiq.vsmarthomehub.core;
 
+import org.json.JSONObject;
+
 public class Model {
+
+    protected JSONObject data;
+
+    public Model(JSONObject data){
+        this.data = data;
+    }
+
+    public JSONObject getData(){
+        return this.data;
+    }
 }
