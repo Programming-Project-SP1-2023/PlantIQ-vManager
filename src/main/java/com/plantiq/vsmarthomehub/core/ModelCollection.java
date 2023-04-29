2f7020e6be177f6988898af0e2ca039c0dab113b
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-29 20:00:21 +1000

    - Updated the ModelCollection class to now input the JSON object to each new Model.

--- src/main/java/com/plantiq/vsmarthomehub/core/ModelCollection.java
+++ src/main/java/com/plantiq/vsmarthomehub/core/ModelCollection.java
@@ -67,7 +67,7 @@ public class ModelCollection {
             JSONArray results = result.getJSONArray("list");
 
             results.forEach((n)->{
-                output.add(new SmartHomeHub(((JSONObject)n).getString("id"),((JSONObject)n).getString("name"),((JSONObject)n).getInt("lastPosted"),((JSONObject)n).getInt("postFrequency"),true,n.toString()));
+                output.add(new SmartHomeHub((JSONObject) n));
             });
         }
 
